cb122624b2a158a816cee8d8969db11c78d00b05
Pavel <76407496+CodeCat6128@users.noreply.github.com>
2024-02-22 15:09:16 +0100

    v0.01

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			'white': {
+				100: '#FFFFFF',
+				200: '#F9F9F9',
+				200: '#F3F8FF',
+			},
+			'blue': {
+				100: '#113A5D',
+				200: '#062743',
+			},
+			'red': '#FF7A8A',
+			'green': {
+				100: '#34d399',
+				200: '#10b981',
+				200: '#059669',
+			},
+		},
+		extend: {
+			boxShadow: {
+				'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+			},
+		},
 	},
 	plugins: [],
 }
