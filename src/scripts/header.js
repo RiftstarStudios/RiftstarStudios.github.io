d3b064423f01b3aba315221974bd349fee4cbd74
Pavel Sheludko <sp6835317@gmail.com>
2024-12-02 22:40:31 +0100

    v0.18.2

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -3,9 +3,11 @@ document.querySelectorAll('#nav-icon, #nav-link').forEach(element => {
         const navIconOpen = document.getElementById('nav-icon-open');
         const navIconClose = document.getElementById('nav-icon-close');
         const menuDiv = document.getElementById('nav-menu');
+        const menuHeader = document.getElementById('nav-header');
         
         navIconOpen.classList.toggle('hidden');
         navIconClose.classList.toggle('hidden');
         menuDiv.classList.toggle('hidden');
+        menuHeader.classList.toggle('bg-opacity-80');
     });
 });
